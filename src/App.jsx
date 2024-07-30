b505161deadf44479bc4a03651e5d50faa966f30
Kingslet Effiong <kingsleyeffiong642@gmail.com>
2024-07-31 00:31:07 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CarRental from "./pages/CarRental";
+import Nav from "./components/NavComp/Nav";
 
 function App() {
   return (
     <BrowserRouter>
+    <Nav/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/rental" element={<CarRental/>}></Route>
